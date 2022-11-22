8b4849f40af39506a00085d8a7144075e8940fba
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-11-22 23:29:16 +0700

    Initial commit

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Main = () => (
-    <h1> Main Goes here</h1>
+    <main >
+        <h1 className='main-title'> Fun facts about React</h1>
+        <ul className='main-items'>
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K star on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        {/* <image scr={reacticon} alt="reactlogo" className='react-bg-logo'/> */}
+    </main>
 
 )
 
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import logo from "../images/logo192.png"
 const Navbar = () => (
-    <h1>
-        Navbar Goes Here
-    </h1>
+    <header>
+        <nav className="nav">
+            <img src={logo} alt="reactlogo" className="nav-logo"/>
+            <h3 className="nav-logo_text">ReactFacts</h3>
+            <h4 className="nav-title">
+                React Course - Project 1
+            </h4>
+        </nav>
+    </header>
 )
 
 export default Navbar
